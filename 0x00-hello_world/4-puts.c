06474e5414ed64ff9457be394f5e3a7e17786e85
Mostafa <mostafa.ashrf20@gmail.com>
2023-07-13 20:11:54 -0700

    First code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: c prog that prints with puts
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
