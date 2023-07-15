b3b76706d95b5c30913cc36267c15be39f63f67a
Mostafa <mostafa.ashrf20@gmail.com>
2023-07-14 19:41:29 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : alphabet
+ *
+ * Return: 0 s
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++
+	}
+	putchar('\n');
+
+	return (0);
+
+}
