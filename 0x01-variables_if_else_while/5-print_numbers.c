932890ea74d245f7ddafba499161545626eda4d0
Mostafa <mostafa.ashrf20@gmail.com>
2023-07-14 19:56:47 -0700

    print nums

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print nums
+ *
+ * Return: 0 s
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%i", num);
+		num++;
+	}
+	printf("\n");
+
+	return (0);
+}
