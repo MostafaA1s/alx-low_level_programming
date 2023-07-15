790e22603bf2fd2ecd1be14cd347644c9d1d1f33
Mostafa <mostafa.ashrf20@gmail.com>
2023-07-14 19:59:05 -0700

    nums with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print nums using putchar
+ *
+ * Return: 0 s
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar("\n");
+
+	return (0);
+}
