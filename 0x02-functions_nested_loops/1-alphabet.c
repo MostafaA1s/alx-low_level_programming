96a13b7245b20c949390f3034df561656b587058
Mostafa <mostafa.ashrf20@gmail.com>
2023-07-18 19:12:00 -0700

    alphabet lower

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ *
+ * Description: print alphabet lower case
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
