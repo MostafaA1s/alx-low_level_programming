3997ceafa468b1d0b134eb255127e79ff582d638
Mostafa <mostafa.ashrf20@gmail.com>
2023-07-20 19:24:06 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multi 2 nums.
+ * @a: first num
+ * @b: second num
+ * Return: mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints nums from 0 to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+
+	return (0);
+}
